b92cfae2ccf39361d157186b54c6a6f5526757ba
ykb <qodqlqodql1206@gmail.com>
2024-11-21 00:01:12 +0900

    header css

--- src/components/HeaderView.vue
+++ src/components/HeaderView.vue
@@ -6,7 +6,8 @@
             <h1></h1>
           </a>
         </div>
-        <nav class="nav">
+        <button class="hamburger" @click="toggleMenu"></button>
+        <nav class="nav" :class="{ open: isMenuOpen }">
           <a href="#intro" data-target="intro">인터뷰</a>
           <a href="#skill" data-target="skill">기술</a>
           <a href="#project" data-target="project">프로젝트</a>
@@ -18,12 +19,19 @@
 <script lang="ts">
 import { ref, onMounted, onUnmounted } from "vue";
 
+
+
 export default {
   setup() {
     const isScrolled = ref(false);
+    const isMenuOpen = ref(false);
 
     const handleScroll = () => {
       isScrolled.value = window.scrollY > 0;
+    };
+
+    const toggleMenu = () => {
+      isMenuOpen.value = !isMenuOpen.value;
     };
 
     onMounted(() => {
@@ -34,7 +42,7 @@ export default {
       window.removeEventListener("scroll", handleScroll);
     });
 
-    return { isScrolled };
+    return { isScrolled, isMenuOpen, toggleMenu };
   },
 };
 
